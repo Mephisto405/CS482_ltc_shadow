13c0fd3610762def91d1bed44e3c96def3c19726
조인영 (In-Young Cho) <ciy405x@kaist.ac.kr>
2018-11-18 17:54:55 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Baseline LTC Source Code Origin
+Copyright (c) 2017, Eric Heitz, Jonathan Dupuy, Stephen Hill and David Neubelt.
+All rights reserved.
+https://github.com/selfshadow/ltc_code
+
+# ltc_code
+Code for "Real-Time Polygonal-Light Shading with Linearly Transformed Cosines"
+
+## WebGL Demos
+[Quad lights](http://blog.selfshadow.com/ltc/webgl/ltc_quad.html)  
+[Line lights](http://blog.selfshadow.com/ltc/webgl/ltc_line.html)  
+[Disk lights](http://blog.selfshadow.com/ltc/webgl/ltc_disk.html)  
---
+++ fit/results/ltc.h
@@ -0,0 +1,30 @@
+struct mat33
+{
+    operator glm::mat3() const
+    {
+        return glm::mat3(m[0], m[1], m[2], m[3], m[4], m[5], m[6], m[7], m[8]);
+    }
+
+    double m[9];
+};
+
+
+#include "ltc.inc"
+
+#if 1
+mat3 M_GGX(const float cosTheta, const float alpha)
+{
+    int t = std::max<int>(0, std::min<int>(size - 1, (int)floorf(sqrtf(1.0f - cosTheta) * size)));
+    int a = std::max<int>(0, std::min<int>(size - 1, (int)floorf(sqrtf(alpha) * size)));
+
+    return tabM[a + t*size];
+}
+
+float magnitude_GGX(const float cosTheta, const float alpha)
+{
+    int t = std::max<int>(0, std::min<int>(size - 1, (int)floorf(sqrtf(1.0f - cosTheta) * size)));
+    int a = std::max<int>(0, std::min<int>(size - 1, (int)floorf(sqrtf(alpha) * size)));
+
+    return tabMagnitude[a + t*size];
+}
+#endif
